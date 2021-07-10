1512b9e42a3047baeb27573460485fbd0fd63304
LukaszPasek <lukasz.pasek7@wp.pl>
2021-07-10 13:52:32 +0200

    App models configuration

---
+++ MusicApp/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  MusicApp
+//
+//  Created by Lukasz Pasek on 01/07/2021.
+//
+
+import Foundation
---
+++ MusicApp/Video.swift
@@ -0,0 +1,8 @@
+//
+//  Video.swift
+//  MusicApp
+//
+//  Created by Lukasz Pasek on 01/07/2021.
+//
+
+import Foundation
