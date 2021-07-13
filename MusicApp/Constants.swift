1c7f459ecf5df8bf0abc2c76adfa5a754674fdd2
LukaszPasek <lukasz.pasek7@wp.pl>
2021-07-13 22:15:28 +0200

    App  configuration

--- MusicApp/Constants.swift
+++ MusicApp/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Constants {
-    static var API_KEY="AIzaSyCo3IhZNV63OILdGc_AWAeUJvC64vshSLE"
+    static var API_KEY=""
     static var PLAYLIST_ID="PLuT98-ulXDNWHEczfUFVnu8WVF80smxKr"
     static var API_URL="https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
